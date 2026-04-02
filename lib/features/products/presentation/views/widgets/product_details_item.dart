ba7d95ea187e35d37bf2c67bd1139814e34161ff
nidhal khazene <nidhal.khazene@gmail.com>
2026-04-02 15:01:32 +0100

    extract product details item

---
+++ lib/features/products/presentation/views/widgets/product_details_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/colors.dart';
+
+class ProductDetailsItem extends StatelessWidget {
+  const ProductDetailsItem({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.trailing,
+  });
+  final Widget title, subtitle, trailing;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 28.5, vertical: 12),
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+          side: BorderSide(color: ColorData.kBorderColor, width: 1),
+        ),
+      ),
+      child: ListTile(
+        title: title,
+        subtitle: Padding(
+          padding: const EdgeInsets.only(top: 4),
+          child: subtitle,
+        ),
+        trailing: SizedBox(width: 35, height: 35, child: trailing),
+      ),
+    );
+  }
+}
