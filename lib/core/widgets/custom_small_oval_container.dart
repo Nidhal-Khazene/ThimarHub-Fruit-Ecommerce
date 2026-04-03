505a5bfbc2a3e3510d3c2a5eec502f430e94e628
nidhal khazene <nidhal.khazene@gmail.com>
2026-04-03 10:48:12 +0100

    custom_small_oval_container: add width, height, padding attribute

--- lib/core/widgets/custom_small_oval_container.dart
+++ lib/core/widgets/custom_small_oval_container.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CustomSmallOvalContainer extends StatelessWidget {
-  const CustomSmallOvalContainer({super.key, required this.widget});
+  const CustomSmallOvalContainer({
+    super.key,
+    required this.widget,
+    this.width,
+    this.height,
+    this.padding,
+  });
   final Widget widget;
+  final double? width;
+  final double? height;
+  final EdgeInsets? padding;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 64,
-      height: 64,
-      padding: const EdgeInsets.all(8),
+      width: width ?? 64,
+      height: height ?? 64,
+      padding: padding ?? const EdgeInsets.all(8),
       decoration: const ShapeDecoration(
         color: Color(0xFFF3F5F7),
         shape: OvalBorder(),
